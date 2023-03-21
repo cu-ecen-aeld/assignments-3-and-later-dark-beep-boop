05843363402f2942085a4134a5faaf3b9419bd5b
zus <Jess Mara Gmez Moreno>
2023-03-21 23:38:30 +0000

    finder script added

---
+++ finder-app/finder.sh
@@ -0,0 +1,19 @@
+
+if [ $# -lt 2 ]; then
+	printf 'Usage: %s <filesdir> <searchstr>\n' $0
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ]; then
+	printf "%s directory doesn't exist or isn't accessible\n" $filesdir
+	exit 1
+fi
+
+filecount=`find $filesdir -type f | wc -l`
+linecount=`grep -R $searchstr $filesdir | wc -l`
+
+printf 'The number of files are %d and the number of matching lines are %d\n' $filecount $matchlines
+
