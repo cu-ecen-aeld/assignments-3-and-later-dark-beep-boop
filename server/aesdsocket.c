d5d03684190a966ad2fe81e06a9a70b5335be492
Jesús María Gómez Moreno <jesus@gomoreno.me>
2023-05-05 01:37:46 +0000

    aesdsocket refactor

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -28,7 +28,7 @@
 #define FILENAME "/var/tmp/aesdsocketdata"
 #define BUFFSIZE 1024
 
-static bool terminate = false;
+static volatile sig_atomic_t terminate = 0;
 
 int
 main(int argc, char *argv[])
@@ -45,27 +45,28 @@ main(int argc, char *argv[])
     }
   }
 
+  if (daemon)
+    TRY(daemonize(), "daemonization failed");
+
   struct sigaction action;
   memset(&action, 0, sizeof action);
   action.sa_handler = terminate_handler;
   sigemptyset(&action.sa_mask);
-  TRYC(sigaddset(&action.sa_mask, SIGTERM));
-  TRYC(sigaddset(&action.sa_mask, SIGINT));
-  TRYC(sigaction(SIGTERM, &action, NULL));
-  TRYC(sigaction(SIGINT, &action, NULL));
-
-  if (daemon)
-    TRY(daemonize());
+  TRYC_ERRNO(sigaddset(&action.sa_mask, SIGTERM));
+  TRYC_ERRNO(sigaddset(&action.sa_mask, SIGINT));
+  TRYC_ERRNO(sigaction(SIGTERM, &action, NULL));
+  TRYC_ERRNO(sigaction(SIGINT, &action, NULL));
 
   int fd;
-  TRYC(
+  TRYC_ERRNO(
       fd = open(
           FILENAME,
           O_RDWR | O_APPEND | O_CREAT,
           S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
 
   int sockfd;
-  TRY(open_listening_socket(&sockfd, PORT, BACKLOG));
+  TRY(open_listening_socket(&sockfd, PORT, BACKLOG),
+      "Couldn't open server socket");
 
   int conn_sockfd;
   char remote_name[INET6_ADDRSTRLEN];
@@ -73,16 +74,15 @@ main(int argc, char *argv[])
   struct sockaddr_storage remote_addr;
 
   while (!terminate) {
-    TRYC_NONBLOCK(
+    TRYC_RETRY_ON_EINTR(
         conn_sockfd =
-            accept(sockfd, (struct sockaddr *) &remote_addr, &addr_size),
-        continue);
+            accept(sockfd, (struct sockaddr *) &remote_addr, &addr_size));
 
-    inet_ntop(
+    TRY_ERRNO(inet_ntop(
         remote_addr.ss_family,
         get_in_addr((struct sockaddr *) &remote_addr),
         remote_name,
-        sizeof remote_name);
+        sizeof remote_name));
     syslog(LOG_DEBUG, "Accepted connection from %s\n", remote_name);
 
     recv_and_send(conn_sockfd, fd);
@@ -110,8 +110,9 @@ recv_and_send(int socket_fd, int file_fd)
 {
   bool ok = false;
 
-  TRY(recv_and_write_line(file_fd, socket_fd));
-  TRY(send_file(socket_fd, file_fd));
+  TRY(recv_and_write_line(file_fd, socket_fd),
+      "line reception or writing failed");
+  TRY(send_file(socket_fd, file_fd), "line reading or sending failed");
 
   ok = true;
 
@@ -125,20 +126,20 @@ daemonize(void)
   bool ok = false;
 
   pid_t daemon_pid;
-  TRYC(daemon_pid = fork());
+  TRYC_ERRNO(daemon_pid = fork());
 
   if (daemon_pid != 0)
     exit(EXIT_SUCCESS);
 
-  TRYC(setsid());
+  TRYC_ERRNO(setsid());
 
-  TRYC(chdir("/"));
+  TRYC_ERRNO(chdir("/"));
 
   for (int i = 0; i < 3; ++i) close(i);
 
-  open("/dev/null", O_RDWR);
-  dup(0);
-  dup(0);
+  TRYC_ERRNO(open("/dev/null", O_RDWR));
+  TRYC_ERRNO(dup(0));
+  TRYC_ERRNO(dup(0));
 
   ok = true;
 
@@ -151,7 +152,6 @@ open_listening_socket(int *sockfd, const char *port, int backlog)
 {
   bool ok = false;
   int sockfd_test;
-  int status;
   int yes = 1;
   struct addrinfo hints;
   struct addrinfo *servinfo = NULL;
@@ -161,15 +161,15 @@ open_listening_socket(int *sockfd, const char *port, int backlog)
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  TRY_GETADDRINFO(getaddrinfo(NULL, PORT, &hints, &servinfo), status);
+  int status;
+  TRY_GETADDRINFO(NULL, PORT, &hints, &servinfo, status);
 
-  TRYC(
-      sockfd_test =
-          socket(servinfo->ai_family, SOCK_STREAM | SOCK_NONBLOCK, 0));
-  TRYC(setsockopt(sockfd_test, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)));
+  TRYC_ERRNO(sockfd_test = socket(servinfo->ai_family, SOCK_STREAM, 0));
+  TRYC_ERRNO(
+      setsockopt(sockfd_test, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)));
 
-  TRYC(bind(sockfd_test, servinfo->ai_addr, servinfo->ai_addrlen));
-  TRYC(listen(sockfd_test, BACKLOG));
+  TRYC_ERRNO(bind(sockfd_test, servinfo->ai_addr, servinfo->ai_addrlen));
+  TRYC_ERRNO(listen(sockfd_test, BACKLOG));
 
   ok = true;
   *sockfd = sockfd_test;
@@ -187,9 +187,10 @@ done:
 void
 terminate_handler(int signo)
 {
-  syslog(LOG_DEBUG, "Caught signal, exiting\n");
-
-  terminate = true;
+  if (signo == SIGTERM || signo == SIGINT) {
+    syslog(LOG_DEBUG, "Caught signal, exiting\n");
+    terminate = 1;
+  }
 }
 
 bool
@@ -198,8 +199,8 @@ recv_and_write_line(int socket_fd, int file_fd)
   bool ok = false;
   char *line = NULL;
 
-  TRY(recv_line(socket_fd, &line));
-  TRY(write_line(file_fd, line));
+  TRY(recv_line(socket_fd, &line), "line reception failed");
+  TRY(write_line(file_fd, line), "line writing failed");
 
   ok = true;
 
@@ -224,7 +225,7 @@ recv_line(int socket_fd, char **line)
     line_buffer = *line;
 
   while (!eol) {
-    TRYC_INTR(bytes_read = recv(socket_fd, buffer, BUFFSIZE, 0), continue);
+    TRYC_RETRY_ON_EINTR(bytes_read = recv(socket_fd, buffer, BUFFSIZE, 0));
     if (bytes_read) {
       ssize_t useful_bytes;
       char *newline_pointer = NULL;
@@ -238,7 +239,8 @@ recv_line(int socket_fd, char **line)
 
       if (useful_bytes > 0) {
         TRY(line_buffer = (char *) realloc(
-                line_buffer, line_buffer_size + useful_bytes + 1));
+                line_buffer, line_buffer_size + useful_bytes + 1),
+            "memory allocation failed");
         strncpy(line_buffer + line_buffer_size, buffer, useful_bytes);
         line_buffer_size += useful_bytes;
       }
@@ -251,7 +253,7 @@ recv_line(int socket_fd, char **line)
   *line = line_buffer;
 
 done:
-  if (!ok && !*line && line_buffer)
+  if (!ok && !(*line) && line_buffer)
     free(line_buffer);
 
   return ok;
@@ -265,10 +267,9 @@ write_line(int file_fd, const char *line)
 
   while (bytes_to_write > 0) {
     ssize_t bytes_written;
-    TRYC_INTR(
+    TRYC_RETRY_ON_EINTR(
         bytes_written = write(
-            file_fd, line + strlen(line) - bytes_to_write, bytes_to_write),
-        continue);
+            file_fd, line + strlen(line) - bytes_to_write, bytes_to_write));
     bytes_to_write -= bytes_written;
   }
 
@@ -286,9 +287,9 @@ send_file(int socket_fd, int file_fd)
   char *line = NULL;
 
   while (!eof) {
-    TRY(read_line(file_fd, &line, &eof));
+    TRY(read_line(file_fd, &line, &eof), "line reading failed");
     if (line && strlen(line)) {
-      TRY(send_line(socket_fd, line));
+      TRY(send_line(socket_fd, line), "line sending failed");
     }
   }
 
@@ -316,7 +317,7 @@ read_line(int file_fd, char **line, bool *eof)
     line_buffer = *line;
 
   while (!eol && !eof_local) {
-    TRYC_INTR(bytes_read = read(file_fd, buffer, BUFFSIZE), continue);
+    TRYC_RETRY_ON_EINTR(bytes_read = read(file_fd, buffer, BUFFSIZE));
     if (bytes_read) {
       ssize_t useful_bytes;
       char *newline_pointer = NULL;
@@ -324,13 +325,14 @@ read_line(int file_fd, char **line, bool *eof)
       if ((newline_pointer = strchr(buffer, '\n')) != NULL) {
         eol = true;
         useful_bytes = newline_pointer - buffer + 1;
-        TRYC(lseek(file_fd, SEEK_CUR, useful_bytes - BUFFSIZE));
+        TRYC_ERRNO(lseek(file_fd, SEEK_CUR, useful_bytes - BUFFSIZE));
       } else {
         useful_bytes = bytes_read;
       }
 
       if (useful_bytes > 0) {
-        TRY(line_buffer = (char *) realloc(
+        TRY_ERRNO(
+            line_buffer = (char *) realloc(
                 line_buffer, line_buffer_size + useful_bytes + 1));
         strncpy(line_buffer + line_buffer_size, buffer, useful_bytes);
         line_buffer_size += useful_bytes;
@@ -348,7 +350,7 @@ read_line(int file_fd, char **line, bool *eof)
   *eof = eof_local;
 
 done:
-  if (!ok && !*line && line_buffer)
+  if (!ok && !(*line) && line_buffer)
     free(line_buffer);
 
   return ok;
@@ -362,10 +364,9 @@ send_line(int socket_fd, const char *line)
 
   while (bytes_to_send > 0) {
     ssize_t bytes_sent;
-    TRYC_INTR(
+    TRYC_RETRY_ON_EINTR(
         bytes_sent = send(
-            socket_fd, line + strlen(line) - bytes_to_send, bytes_to_send, 0),
-        continue);
+            socket_fd, line + strlen(line) - bytes_to_send, bytes_to_send, 0));
     bytes_to_send -= bytes_sent;
   }
 
--- server/try.h
+++ server/try.h
@@ -1,77 +1,70 @@
 #include <errno.h>
 #include <netdb.h>
+#include <stdbool.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <syslog.h>
 
-#define TRYC_INTR(expr, action)                             \
-  if ((expr) == -1) {                                       \
-    if (errno == EINTR) {                                   \
-      action;                                               \
-    } else {                                                \
-      syslog(                                               \
-          LOG_ERR,                                          \
-          "ERROR (file=%s, line=%d, function=%s): %s\n",    \
-          __FILE__,                                         \
-          __LINE__,                                         \
-          __func__,                                         \
-          strerror(errno));                                 \
-      goto done;                                            \
-    }                                                       \
-  } NULL
+#define LOG_ERROR(message)                           \
+  syslog(                                            \
+      LOG_ERR,                                       \
+      "ERROR (file=%s, line=%d, function=%s): %s\n", \
+      __FILE__,                                      \
+      __LINE__,                                      \
+      __func__,                                      \
+      message)
 
-#define TRYC_NONBLOCK(expr, action)                         \
-  if ((expr) == -1) {                                       \
-    if (errno == EAGAIN || errno == EWOULDBLOCK) {          \
-      usleep(5000);                                         \
-      action;                                               \
-    } else {                                                \
-      syslog(                                               \
-          LOG_ERR,                                          \
-          "ERROR (file=%s, line=%d, function=%s): %s\n",    \
-          __FILE__,                                         \
-          __LINE__,                                         \
-          __func__,                                         \
-          strerror(errno));                                 \
-      goto done;                                            \
-    }                                                       \
-  } NULL
+#define TRYCATCH_RETRY(                                            \
+    error_condition, error_action, retry_condition, error_message) \
+  while (error_condition) {                                        \
+    if (!(retry_condition)) {                                      \
+      LOG_ERROR(error_message);                                    \
+      error_action;                                                \
+      break;                                                       \
+    }                                                              \
+  }                                                                \
+  NULL
 
-#define TRY_GETADDRINFO(expr, status)                             \
-  if ((status = expr) != 0) {                                     \
-    syslog(                                                       \
-        LOG_ERR,                                                  \
-        "GETADDRINFO ERROR (file=%s, line=%d, function=%s): %s\n",\
-        __FILE__,                                                 \
-        __LINE__,                                                 \
-        __func__,                                                 \
-        gai_strerror(status));                                    \
-    goto done;                                                    \
-  } NULL
+#define TRY_RETRY(expr, retry_condition, error_message) \
+  TRYCATCH_RETRY(!(expr), goto done, retry_condition, error_message)
 
-#define TRYC(expr)                                      \
-  if ((expr) == -1) {                                   \
-    syslog(                                             \
-        LOG_ERR,                                        \
-        "ERROR (file=%s, line=%d, function=%s): %s\n",  \
-        __FILE__,                                       \
-        __LINE__,                                       \
-        __func__,                                       \
-        strerror(errno));                               \
-    goto done;                                          \
-  } NULL
+#define TRYC_RETRY(expr, retry_condition, error_message) \
+  TRYCATCH_RETRY((expr) == -1, goto done, retry_condition, error_message)
 
-#define TRY(expr)                                       \
-  if ((expr) == 0) {                                    \
-    syslog(                                             \
-        LOG_ERR,                                        \
-        "ERROR (file=%s, line=%d, function=%s): %s\n",  \
-        __FILE__,                                       \
-        __LINE__,                                       \
-        __func__,                                       \
-        #expr);                                         \
-    goto done;                                          \
-  } NULL
+#define TRY_RETRY_ERRNO(expr, retry_condition) \
+  TRY_RETRY(expr, retry_condition, strerror(errno))
 
+#define TRYC_RETRY_ERRNO(expr, retry_condition) \
+  TRYC_RETRY(expr, retry_condition, strerror(errno))
 
+#define TRY_RETRY_ON_EINTR(expr) TRY_RETRY_ERRNO(expr, errno == EINTR)
+#define TRYC_RETRY_ON_EINTR(expr) TRYC_RETRY_ERRNO(expr, errno == EINTR)
+
+#define TRYCATCH(condition, action, message) \
+  if (condition) {                           \
+    LOG_ERROR(message);                      \
+    action;                                  \
+  }                                          \
+  NULL
+
+#define TRY(expr, message) TRYCATCH(!(expr), goto done, message)
+
+#define TRYC(expr, message) TRYCATCH((expr) == -1, goto done, message)
+
+#define TRY_ERRNO(expr) TRY(expr, strerror(errno))
+
+#define TRYC_ERRNO(expr) TRYC(expr, strerror(errno))
+
+#define TRY_GETADDRINFO(node, service, hints, res, status_var)      \
+  if ((status_var = getaddrinfo(node, service, hints, res)) != 0) { \
+    syslog(                                                         \
+        LOG_ERR,                                                    \
+        "GETADDRINFO ERROR (file=%s, line=%d, function=%s): %s\n",  \
+        __FILE__,                                                   \
+        __LINE__,                                                   \
+        __func__,                                                   \
+        gai_strerror(status_var));                                  \
+    goto done;                                                      \
+  }                                                                 \
+  NULL
